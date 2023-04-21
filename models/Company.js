57181e2215ad7960c9ce5a628a0b27f8f9ce6123
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-04-21 18:00:26 -0300

    B-06 and B-07 (#6)

--- models/Company.js
+++ models/Company.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+import { Schema, model, Types } from "mongoose"
 
-let schema = new mongoose.Schema({
+let schema = new Schema({
     name: {type: String, required: true},
     logo: {type: String, required: true},
     website: {type: String, required: true},
     description: {type: String, required: true},
     active: {type: Boolean, required: true},
-    user_id: {type: mongoose.ObjectId, ref: 'users', required: true}
+    user_id: {type: Types.ObjectId, ref: 'users', required: true}
 },{
     timestamps: true,
 })
 let collection = 'companies'
 
-let Company = mongoose.models(schema,collection)
+let Company = model(collection, schema)
 export default Company
