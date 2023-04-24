ddd0146979b0165eed50011e69e1ea0996674457
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-04-24 13:15:13 -0300

    M-02 saved data

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors'
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -18,6 +19,7 @@ import {__dirname} from './utils.js'
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
