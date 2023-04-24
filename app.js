65498fd28a96342751135ab9ad0837135a697481
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-04-24 13:15:51 -0300

    M 02 (#9)
    * M-02
    
    * M-02 saved data

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors'
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -18,6 +19,7 @@ import {__dirname} from './utils.js'
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
