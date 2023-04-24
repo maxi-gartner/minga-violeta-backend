6ecf9be85e6e8a550083cfab0c436d2a986ece16
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-04-24 12:22:50 -0300

    B-09

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,8 +1,19 @@
-let name = 'categories'
+import Category from './../../models/Category.js'
 
-let read = (req,res,next) => res.status(200).render('index',//nombre de la vista
-{//parametros
-    title: '/' +name.toUpperCase(),
-    subtitle: 'endpoints of '+ name
-} )
+let read = async(req,res,next) => { // la funcion debe ser asinmcrona para esperar la respuesta de mongoose
+    try{//utilizo try/catch para intentar algo y sino te devuelve el error
+        let all = await Category.find() // utilizo find() para buscar todos los recursos
+        return res.status(200)
+        .json({
+            categories: all
+        }) //configuro la respuesta que le tengo que enviar al ckliente
+    }catch(error){
+        console.log(error)
+        return res.status(400)
+        .json({
+            error: 'ha ocurrido un error!'
+        })
+
+    }
+}
 export default read
