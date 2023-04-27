cf9827cdd716289aaa0b99094cb757051c43a330
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-04-27 15:40:22 -0300

    :sparkles: change pre string2 (#10)

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,19 +1,20 @@
+import createHttpError from 'http-errors'
 import Category from './../../models/Category.js'
 
-let read = async(req,res,next) => { // la funcion debe ser asinmcrona para esperar la respuesta de mongoose
-    try{//utilizo try/catch para intentar algo y sino te devuelve el error
-        let all = await Category.find() // utilizo find() para buscar todos los recursos
-        return res.status(200)
-        .json({
-            categories: all
-        }) //configuro la respuesta que le tengo que enviar al ckliente
-    }catch(error){
+let read = async (req, res, next) => {          //la funcion controladora debe ser asincrona para poder esperar la respuesta de MONGO
+    try {                                   //utilizo la sintaxis de try/catch para intentar algo y catchear lo errores que puedan surgir
+        let all = await Category.find()     //utilizo el método find() para buscar todos los recursos del modelo (que en este caso es CATEGORY)
+        if (all.length > 0) {
+            return res.status(200)              //configuro la respuesta que le tengo que enviar al cliente (front)
+                .json({
+                    categories: all
+                })
+        }
+        return next(createHttpError(404, 'El recurso no se encontro'))
+    } catch (error) {
         console.log(error)
-        return res.status(400)
-        .json({
-            error: 'ha ocurrido un error!'
-        })
-
+        next(error)
     }
 }
+
 export default read
