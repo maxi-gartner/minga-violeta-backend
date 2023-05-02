3eabd80900e1f346156c58f54c059bc90f909398
Denise Aguirre M <deniseaguirre.m@gmail.com>
2023-05-02 16:51:15 -0300

    restart back

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,7 +1,14 @@
 import {Router} from 'express';
-import read from "../controllers/chapters/read.js"
+
+import read from '../controllers/chapters/read.js';
+import create from '../controllers/chapters/create.js';
+import validator from '../middlewares/validator.js'
+import { createChapterSchema } from '../schemas/chapters.js';
+
+
 let router = Router();
 
 router.get("/", read)
+router.post('/', validator(createChapterSchema) , create)
 
 export default router
