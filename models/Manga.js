aeaac49d63a574e60ea6a29d73282bc1f7de844f
Tomas Salazar <tomas98salazar@gmail.com>
2023-05-03 11:56:36 -0300

    m-03 correccion

--- models/Manga.js
+++ models/Manga.js
@@ -1,10 +1,10 @@
 import { Schema, model, Types} from "mongoose"
 
 let schema = new Schema ({
-    author_id: {type: Types.ObjectId, ref: 'authors', required: false},
+    author_id: {type: Types.ObjectId, ref: 'authors', required: true},
     company_id: {type: Types.ObjectId, ref: 'companies',},
     title: {type: String, required: true},
-    cover_photo: {type: String, required: false},
+    cover_photo: {type: String, required: true},
     description: {type: String, required: true},
     category_id: {type: Types.ObjectId, ref: 'categories',required: true},
 },{
