ec3d698cd15c61e3b64c72671415a5239a8dea02
Denise Aguirre M <deniseaguirre.m@gmail.com>
2023-05-03 18:20:44 -0300

    validation chapterForm

--- routes/chapters.js
+++ routes/chapters.js
@@ -4,11 +4,7 @@ import read from '../controllers/chapters/read.js';
 import create from '../controllers/chapters/create.js';
 import validator from '../middlewares/validator.js'
 import { createChapterSchema } from '../schemas/chapters.js';
-<<<<<<< HEAD
 // import passport from '../middlewares/passport.js';
-=======
-
->>>>>>> edb51f1ce17507dd71b1bf7eeff0330cd43fa8b6
 
 let router = Router();
 
--- schemas/chapters.js
+++ schemas/chapters.js
@@ -3,67 +3,26 @@ import Joi from "joi";
 //objeto de esquema Joi que define la forma de los datos de entrada que se espera para la creación de un capítulo:
 export const createChapterSchema = Joi.object({
   manga_id: Joi.string()
-<<<<<<< HEAD
             .min(4)
-            .required()
-            .messages({
-                    'any.required': 'ID_REQUIRED',
-                    'string.empty': 'ID_REQUIRED',
-                    'string.min': 'ID_TOO_SHORT',
-}),
+            .required(),
+            
   title: Joi.string()
             .min(2)
             .max(30)
-            .required()
-            .messages({
-                      'any.required': 'TITLE_REQUIRED',
-                      'string.empty': 'TITLE_REQUIRED',
-                      'string.min': 'TITLE_TOO_SHORT',
-                      'string.max': 'TITLE_TOO_LARGE',
-}),
+            .required(),
+            
   cover_photo: Joi.string()
-            .uri()
-            .messages({
-                      'any.required': 'COVER_PHOTO_REQUIRED',
-                      'string.empty': 'COVER_PHOTO_REQUIRED',
-                      'string.uri': 'INVALID_URL'
-}),
+            .uri(),
+            
   pages: Joi.array()
             .items(Joi.string().uri())
             .required()
             .messages({
-                      'any.required': 'PAGES_REQUIRED',
-                      'string.empty': 'PAGES_REQUIRED',
-                      'string.uri': 'INVALID_URL'
+                      'string.empty': '"pages" is not allowed to be empty',
+                      'string.uri': '"pages" must be a valid uri'
 }),
   order: Joi.number()
             .integer()
             .min(1)
             .required()
-            .messages({
-                      'any.required': 'ORDER_REQUIRED',
-                      'string.empty': 'ORDER_REQUIRED',
-                      'string.number': 'ORDER_NUMBER_REQUIRED'
-})
-=======
-  .min(4)
-  .required()
-  .message({
-    "string.min": "manga_id required "
-  }),
-
-  title: Joi.string()
-  .min(2)
-  .max(30)
-  .required(),
-  cover_photo: Joi.string()
-  .uri(),
-  pages: Joi.array()
-  .items(Joi.string().uri())
-  .required(),
-  order: Joi.number()
-  .integer()
-  .min(1)
-  .required(),
->>>>>>> edb51f1ce17507dd71b1bf7eeff0330cd43fa8b6
 });
