8ef13c80c692bd30b94c809ea453ab3d0f8bbd98
Denise Aguirre Martinez <31725325+DeniseAguirre@users.noreply.github.com>
2023-05-03 10:25:25 -0300

    restart back (#12)
    Co-authored-by: Denise Aguirre M <deniseaguirre.m@gmail.com>

---
+++ middlewares/passport.js
@@ -0,0 +1,25 @@
+import passport from "passport"
+import passportJwt from "passport-jwt"
+
+
+passport.use(
+    new passportJwt.Strategy({
+        jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.SECRET
+    },				
+    async (jwt_payload,done) => {
+        try {				
+            let user = await User.findOne({_id:jwt_payload.id})
+            if (user) {		
+                return done(null, user)
+            } else {
+                return done(null, false)
+            }
+        } catch (error) {
+            console.log(error)
+            return done(error,false)
+        }
+    }
+))
+
+export default passport
--- models/Chapter.js
+++ models/Chapter.js
@@ -3,9 +3,9 @@ import { Schema, model, Types} from "mongoose"
 let schema = new Schema ({
     manga_id: { type: Types.ObjectId, ref:'mangas' ,required: true},
     title: { type: String, required: true},
-    cover_photo: { type: String, required: true},
+    cover_photo: { type: String, required: false},
     pages: [{ type: String, required: true}],
-    order: { type: String, required: true},
+    order: { type: Number, required: true},
 },{
     timestamps: true,
 
