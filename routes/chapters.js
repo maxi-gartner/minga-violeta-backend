b17d2eae6e8dca1292135f5f09b295b633762d80
Denise Aguirre Martinez <31725325+DeniseAguirre@users.noreply.github.com>
2023-05-04 13:21:41 -0300

    M 04 02 (#19)
    * restart back
    
    * changes joi
    
    * validation chapterForm
    
    * M-04
    
    ---------
    
    Co-authored-by: Denise Aguirre M <deniseaguirre.m@gmail.com>

--- routes/chapters.js
+++ routes/chapters.js
@@ -4,11 +4,12 @@ import read from '../controllers/chapters/read.js';
 import create from '../controllers/chapters/create.js';
 import validator from '../middlewares/validator.js'
 import { createChapterSchema } from '../schemas/chapters.js';
+import chapterExistsCreate from '../middlewares/chapterExistCreate.js'; 
 // import passport from '../middlewares/passport.js';
 
 let router = Router();
 
 router.get("/", read)
-router.post('/', validator(createChapterSchema) , create)
+router.post('/', /* passport.authenticate('jwt', {session: false}), */ validator(createChapterSchema), chapterExistsCreate, create)
 
 export default router
