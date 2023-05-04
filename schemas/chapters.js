f5ff7636bb969637b96de6fce1afd179047923b7
Denise Aguirre M <deniseaguirre.m@gmail.com>
2023-05-04 13:20:01 -0300

    M-04

--- schemas/chapters.js
+++ schemas/chapters.js
@@ -5,25 +5,50 @@ export const createChapterSchema = Joi.object({
   
   manga_id: Joi.string()
             .min(4)
-            .required(),
+            .required()
+            .messages({
+              'string.base': `"manga_id" should be a string`,
+              'string.min': `"manga_id" should have a minimum length of {#limit}`,
+              'any.required': `"manga_id" is required`
+            }),
   
   title: Joi.string()
             .min(2)
             .max(30)
-            .required(),
+            .required()
+            .messages({
+              'string.base': `"title" should be a string`,
+              'string.min': `"title" should have a minimum length of {#limit}`,
+              'string.max': `"title" should have a maximum length of {#limit}`,
+              'any.required': `"title" is required`
+            }),
             
   cover_photo: Joi.string()
-            .uri(),
+            .uri()
+            .messages({
+              'string.base': `"cover_photo" should be a string`,
+              'string.uri': `"cover_photo" should be a valid uri`
+            }),
             
   pages: Joi.array()
             .items(Joi.string().uri())
             .required()
             .messages({
-                      'string.empty': '"pages" is not allowed to be empty',
-                      'string.uri': '"pages" must be a valid uri'
-}),
+              'any.required': `"pages" is required`,
+              'array.base': `"pages" should be an array`,
+              'array.includesRequiredUnknowns': `"pages" must contain at least one valid uri`,
+              'string.empty': `"pages" should not be empty`,
+              'string.uri': `"pages" should be a valid uri`
+            }),
+            
   order: Joi.number()
             .integer()
             .min(1)
             .required()
+            .messages({
+              'number.base': `"order" should be a number`,
+              'number.integer': `"order" should be an integer`,
+              'number.min': `"order" should be greater than or equal to {#limit}`,
+              'any.required': `"order" is required`
+            })
 });
