359b79a343b744674a9b9e29f90636344375a1b1
Tomas Salazar <tomas98salazar@gmail.com>
2023-05-09 15:29:45 -0300

    m-07

--- routes/mangas.js
+++ routes/mangas.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
-import read from "../controllers/mangas/read.js"
+import read from "../controllers/mangas/get_mangas.js"
 import create from '../controllers/mangas/create.js'
 import validator from '../middlewares/validator.js'
 import { mangasCreate } from '../schemas/mangas.js';
 import titleExistsCreate from '../middlewares/exists_title.js.js';
 let router = Router();
 
-router.get("/", read)
+router.get('/', read)
 router.post('/', validator(mangasCreate), titleExistsCreate, create)
 
 export default router
