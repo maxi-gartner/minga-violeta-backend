f3f000113a972aca4519f077d4ee679fc8070ebc
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-05-10 12:01:15 -0300

    M 08 01 (#29)
    * :sparkles: add query params read mangas
    
    * add get_one and get_chapters
    
    * delet changes read auth and chapters
    
    * delet changes read mangas
    
    * delet pages in findOne chapter
    
    * finish m-08-01
    
    * add passport routes
    
    * delet old read
    
    * :fire: detelet old code

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,15 +1,17 @@
 import {Router} from 'express';
 
+import get_chapters from '../controllers/chapters/get_chapters.js';
 import read from '../controllers/chapters/read.js';
 import create from '../controllers/chapters/create.js';
 import validator from '../middlewares/validator.js'
 import { createChapterSchema } from '../schemas/chapters.js';
 import chapterExistsCreate from '../middlewares/chapterExistCreate.js'; 
-// import passport from '../middlewares/passport.js';
+import passport from '../middlewares/passport.js';
 
 let router = Router();
 
-router.get("/", read)
-router.post('/', /* passport.authenticate('jwt', {session: false}), */ validator(createChapterSchema), chapterExistsCreate, create)
+router.get("/?manga_id",passport.authenticate('jwt', {session: false}), read)
+router.get("/", get_chapters)
+router.post('/', passport.authenticate('jwt', {session: false}), validator(createChapterSchema), chapterExistsCreate, create)
 
 export default router
