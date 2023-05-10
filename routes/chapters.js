cfc7eaa345c317e3cad7d3ab051b216e407b8a99
Denise Aguirre Martinez <31725325+DeniseAguirre@users.noreply.github.com>
2023-05-10 12:33:05 -0300

    M 09 (#22)
    * Add get-one.js and endpoint
    
    * verify one
    
    * fixed get-one.js
    
    ---------
    
    Co-authored-by: Denise Aguirre M <deniseaguirre.m@gmail.com>
    Co-authored-by: Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>

--- routes/chapters.js
+++ routes/chapters.js
@@ -7,10 +7,12 @@ import validator from '../middlewares/validator.js'
 import { createChapterSchema } from '../schemas/chapters.js';
 import chapterExistsCreate from '../middlewares/chapterExistCreate.js'; 
 import passport from '../middlewares/passport.js';
+import one from '../controllers/chapters/get_one.js';
 
 let router = Router();
 
-router.get("/?manga_id",passport.authenticate('jwt', {session: false}), read)
+router.get('/:id', passport.authenticate('jwt', {session: false}), one)
+router.get("/?manga_id", passport.authenticate('jwt', {session: false}), read)
 router.get("/", get_chapters)
 router.post('/', passport.authenticate('jwt', {session: false}), validator(createChapterSchema), chapterExistsCreate, create)
 
