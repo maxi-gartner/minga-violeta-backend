93fc98077e0194182de15ef1d8a12c88dc34dcb3
Tomas Salazar <tomas98salazar@gmail.com>
2023-05-10 11:56:46 -0300

    m-07 correcciones

--- routes/mangas.js
+++ routes/mangas.js
@@ -4,9 +4,12 @@ import create from '../controllers/mangas/create.js'
 import validator from '../middlewares/validator.js'
 import { mangasCreate } from '../schemas/mangas.js';
 import titleExistsCreate from '../middlewares/exists_title.js.js';
+import passport from 'passport';
 let router = Router();
 
-router.get('/', read)
-router.post('/', validator(mangasCreate), titleExistsCreate, create)
+/* router.get('/', read)
+router.post('/', validator(mangasCreate), titleExistsCreate, create) */
+router.get('/', passport.authenticate('jwt', {session: false}), read)
+router.post('/', passport.authenticate('jwt', {session: false}), validator(mangasCreate), titleExistsCreate, create)
 
 export default router
--- schemas/mangas.js
+++ schemas/mangas.js
@@ -10,10 +10,10 @@ export const mangasCreate = joi.object({
         .min(3)
         .max(30)
         .messages({
-            'any.required': 'TITLE_REQUIRED',
-            'string.empty': 'TITLE_REQUIRED',
-            'string.min': 'TITLE_TOO_SHORT',
-            'string.max': 'TITLE_TOO_LARGE'
+            'any.required': 'Title required',
+            'string.empty': 'Title cant be empty',
+            'string.min': 'Title is too short',
+            'string.max': 'Title is too large'
         }),
 
     cover_photo: joi.string(),
@@ -23,17 +23,17 @@ export const mangasCreate = joi.object({
         .min(10)
         .max(300)
         .messages({
-            'any.required': 'DESCRIPTION_REQUIRED',
-            'string.empty': 'DESCRIPTION_REQUIRED',
-            'string.min': 'DESCRIPTION_TOO_SHORT',
-            'string.max': 'DESCRIPTION_TOO_LARGE'
+            'any.required': 'Description is required',
+            'string.empty': 'Description cant be empty',
+            'string.min': 'Description is too short',
+            'string.max': 'Description is too large'
         }),
 
     category_id: joi.string()
         .required()
         .messages({
-            'any.required': 'CATEGORY_REQUIRED',
-            'string.empty': 'CATEGORY_REQUIRED'
+            'any.required': 'Category is required',
+            'string.empty': 'Category cant be empty'
         }),
 
 })
