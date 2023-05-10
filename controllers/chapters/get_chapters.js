db244456013423be2b2c427dbdec232920f9bf73
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-05-10 11:32:20 -0300

    :fire: detelet old code

--- controllers/chapters/get_chapters.js
+++ controllers/chapters/get_chapters.js
@@ -1,24 +1,3 @@
-/* import createHttpError from 'http-errors'
-import Chapter from '../../models/Chapter.js'
-
-let one = async (req, res, next) => {
-    try {
-        let {manga_id, page} = req.params
-        let one = await Chapter.findOne({
-            manga_id: manga_id, page: 
-            }, "order title cover_photo -_id")
-        return res.status(200).json({
-            success: 'ok',
-            response: one
-        })
-    } catch (error) {
-        console.log(error)
-        next(error)
-    }
-}
-
-export default one */
-
 import createHttpError from 'http-errors'
 import Chapter from './../../models/Chapter.js'
 
