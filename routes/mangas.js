d31a925cf09e588a10ceb503949939112bfa774d
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-05-19 11:26:47 -0300

    m-10-05

--- routes/mangas.js
+++ routes/mangas.js
@@ -12,13 +12,14 @@ import update from '../controllers/mangas/update.js'
 import destroy from '../controllers/mangas/destroy.js'
 import is_active from '../middlewares/is_active.js'
 import is_property_of from '../middlewares/is_property_of.js'
+import { mangaEdit } from '../schemas/mangaEdit.js';
 
 let router = Router();
 router.get("/me", passport.authenticate('jwt', {session: false}), finds_id, get_me)
 router.get('/', passport.authenticate('jwt', {session: false}), read)
 router.post('/', passport.authenticate('jwt', {session: false}), validator(mangasCreate), titleExistsCreate, create)
 router.get("/:id",passport.authenticate('jwt', {session: false}), get_one)
-router.put("/:id",passport.authenticate('jwt', {session: false}), finds_id, is_active, is_property_of, update)
+router.put("/:id",passport.authenticate('jwt', {session: false}), validator(mangaEdit),finds_id, is_active, is_property_of,  update)
 router.delete("/:id",passport.authenticate('jwt', {session: false}), finds_id, is_active, is_property_of, destroy)
 
 export default router
