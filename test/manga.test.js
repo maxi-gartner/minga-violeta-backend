c350ef201d79bb36e115ba1688233b57c7ad12e5
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-05-26 13:39:43 -0300

    add readmeTesting

--- test/manga.test.js
+++ test/manga.test.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import request from "supertest";
 import app from "../app.js"
 
-describe("Test on /mangas path", () =>{
+describe("Test post and delete /mangas path", () =>{
 
     let server = null;
     let api = null;
